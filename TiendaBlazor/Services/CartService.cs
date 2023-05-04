2a6617eaae51fffa5e4e8f7031f32497eba4893d
Amanda VAZQUEZ MOLINA <avazquezmolina@cegid.com>
2023-05-04 11:44:46 +0200

    Add project files.

---
+++ TiendaBlazor/Services/CartService.cs
@@ -0,0 +1,67 @@
+﻿// CartService.cs
+using System.Collections.Generic;
+using TiendaBlazor.Models;
+
+namespace TiendaBlazor.Services
+{
+    public class CartService
+    {
+        public List<CartItem> CartItems { get; } = new List<CartItem>();
+
+        // Agrega un producto al carrito de compras
+        public void AddToCart(Product product)
+        {
+            // Buscar si el producto ya está en el carrito
+            var existingCartItem = CartItems.Find(item => item.Product.Id == product.Id);
+
+            if (existingCartItem != null)
+            {
+                // Si el producto ya está en el carrito, incrementar la cantidad
+                existingCartItem.Quantity++;
+            }
+            else
+            {
+                // Si el producto no está en el carrito, agregar un nuevo item al carrito
+                var newCartItem = new CartItem { Product = product, Quantity = 1 };
+                CartItems.Add(newCartItem);
+            }
+        }
+
+        public void RemoveFromCart(Product product)
+        {
+            // Buscar el item del carrito correspondiente al producto a eliminar
+            var itemToRemove = CartItems.Find(item => item.Product.Id == product.Id);
+
+            if (itemToRemove != null)
+            {
+                if (itemToRemove.Quantity > 1)
+                {
+                    // Si la cantidad es mayor a 1, disminuir la cantidad
+                    itemToRemove.Quantity--;
+                }
+                else
+                {
+                    // Si la cantidad es 1, remover el item del carrito
+                    CartItems.Remove(itemToRemove);
+                }
+            }
+        }
+
+        // Obtiene la cantidad total de precio de los productos en el carrito de compras
+        public decimal GetTotalItemCount()
+        {
+            decimal total = 0;
+            foreach (var item in CartItems)
+            {
+                total += item.Product.Price * item.Quantity;
+            }
+            return total;
+        }
+
+        // Obtiene la lista de items en el carrito de compras
+        public List<CartItem> GetCartItems()
+        {
+            return CartItems;
+        }
+    }
+}
---
+++ TiendaBlazor/Services/JsonDataService.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using TiendaBlazor.Models;
+
+namespace TiendaBlazor.Services
+{
+    public class JsonDataService
+    {
+
+        public JsonDataService()
+        {
+            
+        }
+
+        public List<Product> GetProducts()
+        {
+            var products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Producto 1", Price = 100, Description = "Descripción del producto 1", CategoryId = 1, Image="imgs/img2.rtve.jpg" },
+        new Product { Id = 2, Name = "Producto 2", Price = 200, Description = "Descripción del producto 2", CategoryId = 1, Image="imgs/img2.rtve.jpg" },
+        new Product { Id = 3, Name = "Producto 3", Price = 300, Description = "Descripción del producto 3", CategoryId = 2, Image="imgs/img2.rtve.jpg" },
+        new Product { Id = 4, Name = "Producto 4", Price = 400, Description = "Descripción del producto 4", CategoryId = 2, Image="imgs/img2.rtve.jpg" },
+        new Product { Id = 5, Name = "Producto 5", Price = 500, Description = "Descripción del producto 5", CategoryId = 3, Image="imgs/img2.rtve.jpg" },
+        new Product { Id = 6, Name = "Producto 6", Price = 600, Description = "Descripción del producto 6", CategoryId = 3, Image="imgs/img2.rtve.jpg" },
+        new Product { Id = 7, Name = "Producto 7", Price = 400, Description = "Descripción del producto 7", CategoryId = 2, Image="imgs/img2.rtve.jpg" },
+        new Product { Id = 8, Name = "Producto 8", Price = 500, Description = "Descripción del producto 8", CategoryId = 3, Image="imgs/img2.rtve.jpg" },
+        new Product { Id = 9, Name = "Producto 9", Price = 600, Description = "Descripción del producto 9", CategoryId = 3, Image="imgs/img2.rtve.jpg" },
+    };
+
+            return products;
+        }
+
+        public List<Category> GetCategories()
+        {
+            var category = new List<Category>
+           {
+                new Category { Id = 1, Name = "Category 1" },
+                new Category { Id = 2, Name = "Category 2" },
+                new Category { Id = 3, Name = "Category 3" }
+           };
+            return category;
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            var customer = new List<Customer>
+            {
+                 new Customer { Id = 1, Name = "Customer 1", Email = "customer1@example.com" },
+                 new Customer { Id = 2, Name = "Customer 2", Email = "customer2@example.com" }
+            };
+            return customer;
+           
+        }
+
+    }
+}
